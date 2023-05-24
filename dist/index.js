e29b663c539cc93fe56050bf3c5a0e1c0e3d0aef
piyaponSae <piyapon.sae@gmail.com>
2023-05-24 11:08:07 +0700

    Finish problem-2

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,21 @@
 "use strict";
 function getClockAngle(hh_mm) {
-  const time = hh_mm;
-  const h = parseInt(time.split(":")[0]);
-  const m = parseInt(time.split(":")[1]);
-  const angle = Math.abs((11 / 2) * m - 30 * h);
-  console.log(angle);
+    const time = hh_mm;
+    const hour = parseInt(time.split(":")[0]);
+    const minute = parseInt(time.split(":")[1]);
+    let angle = Math.abs((11 * minute) / 2 - 30 * hour) % 360;
+    if (angle <= 180) {
+        console.log("Shorter angle:", angle);
+        return angle;
+    }
+    else if (angle > 180) {
+        console.log("Shorter angle:", 360 - angle);
+        return 360 - angle;
+    }
+    else {
+        console.log("Shorter angle:", 180);
+        return 180;
+    }
 }
+getClockAngle("10:00");
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
 // Formula: angle = |(11m/2) - 30h|
 
-function getClockAngle(hh_mm: string): void {
+function getClockAngle(hh_mm: string): number {
   const time = hh_mm;
-  const h: number = parseInt(time.split(":")[0]);
-  const m: number = parseInt(time.split(":")[1]);
-  const angle: number = Math.abs((11 / 2) * m - 30 * h);
+  const hour: number = parseInt(time.split(":")[0]);
+  const minute: number = parseInt(time.split(":")[1]);
+  let angle: number = Math.abs((11 * minute) / 2 - 30 * hour) % 360;
+
+  if (angle <= 180) {
+    console.log("Shorter angle:", angle);
+    return angle;
+  } else if (angle > 180) {
+    console.log("Shorter angle:", 360 - angle);
+    return 360 - angle;
+  } else {
+    console.log("Shorter angle:", 180);
+    return 180;
+  }
 }
 
-getClockAngle("09:15");
+getClockAngle("10:00");
